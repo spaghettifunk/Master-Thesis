8b9515689e6bdb13611522725bf4b23bf811a564
davideberdin <davide.berdin.0110@student.uu.se>
2015-09-08 11:41:49 -0400

    modified gitignore

---
+++ Project/DNN/audiohandler.py
@@ -0,0 +1,60 @@
+'''
+The MIT License
+
+Copyright (c) 2015 University of Rochester, Uppsala University
+Authors: Davide Berdin, Philip J. Guo, Olle Galmo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+
+import sys
+import os
+from scipy.io import wavfile
+
+
+# Class for reading files from a folder and save them in a specific structure
+class AudioHandler:
+    # Constants
+    audio_files_directory = "audio-data/"
+
+    # structure containing all the audio signals
+    audio_data = {}
+
+    # Constructor
+    def __init__(self):
+        self.audio_data = self.read_data_from_folder()
+
+    def read_data_from_folder(self):
+        all_signals = []
+
+        for root, dirs, files in os.walk(self.audio_files_directory):
+            for audio in files:
+                file_name = os.path.join(root, audio)
+
+                if '.DS_Store' in file_name:
+                    continue
+
+                try:
+                    fs, signal_data = wavfile.read(file_name)
+                    self.audio_data[file_name] = signal_data
+                except:
+                    print("Unexpected error:", sys.exc_info()[0])
+                    raise
+
+        return all_signals
