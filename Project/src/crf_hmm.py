b0cd91219162a23e63b689fca060a6232a6c03c6
davideberdin <davide.berdin.0110@student.uu.se>
2015-10-07 18:23:59 -0400

    working with different model

--- Project/src/crf_hmm.py
+++ Project/src/crf_hmm.py
@@ -28,11 +28,13 @@ import yaml
 import csv
 import os
 import numpy as np
+from time import time
+from copy import deepcopy
 
 # models and trainers
 from sklearn.svm import LinearSVC
-from pystruct.models import GraphCRF
-from pystruct.learners import FrankWolfeSSVM
+from pystruct.models import GraphCRF, ChainCRF
+from pystruct.learners import FrankWolfeSSVM, NSlackSSVM
 
 
 class Features:
@@ -84,10 +86,13 @@ class Features:
 class CRF_HMM:
     train_dictionary_phonemes_directory = "output-data/train_audio_phonemes_labels.txt"
     test_dictionary_phonemes_directory = "output-data/test_audio_phonemes_labels.txt"
-    csv_directory = "output-data/smoothed-csv-files/train/"
+    train_csv_directory = "output-data/train-smoothed-csv-files/"
+    test_csv_directory = "output-data/test-smoothed-csv-files/"
 
-    X_train = []
-    y_train = []
+    X_train = []    # sample for training
+    y_train = []    # training labels
+    X_test = []     # samples for testing
+    y_test = []     # testing labels
 
     dictionary_trainset = {}
     dictionary_testset = {}
@@ -100,10 +105,15 @@ class CRF_HMM:
         with open(self.train_dictionary_phonemes_directory) as data_file:
             self.dictionary_trainset = yaml.load(data_file)
 
-    def load_trainig_set(self):
+    def load_trainig_set(self, isTest=False):
+        if isTest:
+            csv_directory = self.test_csv_directory
+        else:
+            csv_directory = self.train_csv_directory
+
         counter = 0
-        for filename in os.listdir(self.csv_directory):
-            file_directory = os.path.join(self.csv_directory, filename)
+        for filename in os.listdir(csv_directory):
+            file_directory = os.path.join(csv_directory, filename)
 
             if ".DS_Store" in filename:
                 continue
@@ -129,33 +139,58 @@ class CRF_HMM:
                     print "Error: ", sys.exc_info()
                     raise
 
-            for i in range(5):
-                if i == 0:
+                if isTest:
+                    phonemes_key = filename.replace('.csv', '.TextGrid')
+                    phonemes_values = self.dictionary_testset[phonemes_key]
+                    self.X_test.append(deepcopy(feat.get_matrix()))
+
+                    # need to fill the array with 0s in order to have
+                    # 161 "labels"
+                    initial_arr_length = len(phonemes_values)
+                    for i in range(161):
+                        if i < initial_arr_length:
+                            continue
+                        phonemes_values.append(0)
+
+                    self.y_test.append(deepcopy(np.array(phonemes_values)))
                     continue
+                else:
+                    for i in range(5):
+                        if i == 0:
+                            continue
 
-                temp = filename
-                phonemes_key = filename.replace('.csv', '')
-                phonemes_key = phonemes_key + '_' + str(i) + '.TextGrid'
-                phonemes_values = self.dictionary_trainset[phonemes_key]
+                        temp = filename
+                        phonemes_key = filename.replace('.csv', '')
+                        phonemes_key = phonemes_key + '_' + str(i) + '.TextGrid'
+                        phonemes_values = self.dictionary_trainset[phonemes_key]
 
-                initial_arr_length = len(phonemes_values)
-                for i in range(161):
-                    if i < initial_arr_length:
-                        continue
-                    phonemes_values.append(0)
+                        initial_arr_length = len(phonemes_values)
+                        for i in range(161):
+                            if i < initial_arr_length:
+                                continue
+                            phonemes_values.append(0)
 
-                self.X_train.append(feat.get_matrix())
-                self.y_train.append(np.array(phonemes_values))
-                counter += 1
-                filename = temp
+                        self.X_train.append(deepcopy(feat.get_matrix()))
+                        self.y_train.append(deepcopy(np.array(phonemes_values)))
+
+                        counter += 1
+                        filename = temp
 
     def train_model(self):
         try:
 
-            svm = LinearSVC(dual=False, C=.1)
-            svm.fit(np.vstack(self.X_train), np.hstack(self.y_train))
+            # this model works!
+            svm = LinearSVC(dual=True, C=1.0, verbose=0, max_iter=1000, random_state=3)
 
-            print("Test score with chain CRF: %f" % svm.score(np.vstack(self.X_train), np.hstack(self.y_train)))
+            start = time()
+            svm.fit(np.vstack(self.X_train), np.hstack(self.y_train))
+            time_svm = time() - start
+
+            prediction = svm.predict(np.vstack(self.X_test[1]))
+
+            print "Time: %f seconds", time_svm
+            print "Score: %f" % svm.score(np.vstack(self.X_test), np.hstack(self.y_test))
+            print "Prediction: ", prediction
 
         except:
             print "Error: ", sys.exc_info()
@@ -165,6 +200,7 @@ class CRF_HMM:
         self.load_train_phonemes_dictionary()
         self.load_test_phonemes_dictionary()
         self.load_trainig_set()
+        self.load_trainig_set(True)
         self.train_model()
 
 
