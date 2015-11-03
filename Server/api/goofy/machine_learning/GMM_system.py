45f0cdb2e55e5b7ba67ea8ae82bf3048d81e2865
davideberdin <davide.berdin.0110@student.uu.se>
2015-11-03 16:51:55 -0500

    feedbacks page almost completed

---
+++ Server/api/goofy/machine_learning/GMM_system.py
@@ -0,0 +1,233 @@
+"""
+The MIT License
+
+Copyright (c) 2015 University of Rochester, Uppsala University
+Authors: Davide Berdin, Philip J. Guo, Olle Galmo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+import os
+import sys
+import csv
+import numpy as np
+import cPickle
+import base64
+
+import matplotlib.pyplot as plt
+
+from sklearn import mixture
+from copy import deepcopy
+
+from prepare_data import GMM_structure
+
+
+class GMM_prototype:
+    # region Global variables
+    male_formants_files_directory = 'data/formants_results/male/'
+    female_formants_files_directory = 'data/formants_results/female/'
+    male_model_name = 'models/gmm_male_model.pkl'
+    female_model_name = 'models/gmm_female_model.pkl'
+
+    male_trainset_name = '/models/trainset_male.pkl'
+    female_trainset_name = '/models/trainset_female.pkl'
+    # endregion
+
+    # Train model with GMM
+    def create_structure(self, isFemale=False):
+        if isFemale:
+            self.formants_files_directory = self.female_formants_files_directory
+        else:
+            self.formants_files_directory = self.female_formants_files_directory
+
+        all_data = []
+
+        path = os.path.dirname(os.path.abspath(__file__))
+        formants_files = os.path.join(path, self.formants_files_directory)
+        os.chdir(formants_files)
+
+        for filename in os.listdir("."):
+
+            if ".DS_Store" in filename or "_norm" not in filename:
+                continue
+
+            with open(filename, 'r') as tabbed_file:
+                reader = csv.reader(tabbed_file, delimiter="\t")
+                all_lines = list(reader)
+
+                data = GMM_structure(file)
+
+                not_included = 0
+                for l in all_lines:
+                    if not_included <= 2:
+                        not_included += 1
+                        continue
+
+                    data.set_object(0, l[0])
+                    data.set_object(1, l[1])
+                    data.set_object(2, l[2])
+                    try:
+                        if l[3] == '':
+                            f1_val = 0.0
+                        else:
+                            f1_val = float(l[3])
+
+                        if l[4] == '':
+                            f2_val = 0.0
+                        else:
+                            f2_val = float(l[4])
+
+                        data.set_object(3, f1_val)
+                        data.set_object(4, f2_val)
+                    except:
+                        print "Error: ", sys.exc_info()
+
+                all_data.append(deepcopy(data))
+        return all_data
+
+    def train_GMM(self, isFemale=False):
+        data = self.create_structure(isFemale)
+
+        path = os.path.dirname(os.path.abspath(__file__))
+
+        all_vowels = []
+        all_norm_f1 = []
+        all_norm_f2 = []
+
+        for str in data:
+            temp_vowels = np.array(str.get_object(0))
+            temp_norm_f1 = np.array(str.get_object(3))
+            temp_norm_f2 = np.array(str.get_object(4))
+
+            all_vowels.extend(temp_vowels)
+            all_norm_f1.extend(temp_norm_f1)
+            all_norm_f2.extend(temp_norm_f2)
+
+        try:
+            X_train = []
+            for f, b in zip(all_norm_f1, all_norm_f2):
+                X_train.append([f, b])
+
+            X_train = np.array(X_train)
+            Y_train = np.vstack(all_vowels)
+
+            if isFemale:
+                path_trainset = path + self.female_trainset_name
+            else:
+                path_trainset = path + self.male_trainset_name
+
+            with open(path_trainset, 'wb') as fid:
+                cPickle.dump([X_train, Y_train], fid)
+
+            n_classes = len(np.unique(Y_train))
+            gmm_classifier = mixture.GMM(n_components=n_classes, covariance_type='full')
+            gmm_classifier.fit(X_train)
+
+            # save the classifier
+            if isFemale:
+                model_name = self.female_model_name
+            else:
+                model_name = self.male_model_name
+
+            model_directory = os.path.join(path, model_name)
+
+            with open(model_directory, 'wb') as fid:
+                cPickle.dump(gmm_classifier, fid)
+
+        except:
+            print sys.exc_info()
+            raise
+
+    def test_GMM(self, X_test, Y_test, plot_filename, isFemale=False):
+
+        path = os.path.dirname(os.path.abspath(__file__))
+        path += '/'
+
+        if isFemale:
+            model_name = path + self.female_model_name
+            trainset_name = path + self.female_trainset_name
+        else:
+            model_name = path + self.male_model_name
+            trainset_name = path + self.male_trainset_name
+
+        # load it again
+        with open(model_name, 'rb') as model:
+            gmm_classifier = cPickle.load(model)
+
+        with open(trainset_name, 'rb') as traindata:
+            X_train, Y_train = cPickle.load(traindata)
+
+        # results
+        labels = np.unique(Y_train)
+        int_labels = np.arange(len(labels))
+
+        map_int_label = dict(zip(int_labels, labels))
+        map_label_int = dict(zip(labels, int_labels))
+
+        gmm_logprob, gmm_resp = gmm_classifier.score_samples(X_test)
+        gmm_res = sum(gmm_classifier.score(X_test))
+        gmm_predict = gmm_classifier.predict(X_test)
+        gmm_predict_proba = gmm_classifier.predict_proba(X_test)
+
+        try:
+            fig = plt.figure()
+            ax1 = fig.add_subplot(111)
+
+            predicted_labes = []
+            for pred in gmm_predict:
+                predicted_labes.append(map_int_label[pred])
+
+            Y_test_int = []
+            for k in Y_test:
+                Y_test_int.append(map_label_int[k[0]])
+
+            test_accuracy = np.mean(gmm_predict.ravel() == np.array(Y_test_int).ravel()) * 100
+
+            # print the predicted-vowels based on the formants
+            for _s, _x, _y in zip(predicted_labes, X_test[:, 0], X_test[:, 1]):
+                ax1.scatter(_x, _y, s=400, c='r', marker=r"$ {} $".format(_s))
+
+            # predict the native set for that sentence
+            for _s, _x, _y in zip(map_label_int, X_train[:, 0], X_train[:, 1]):
+                ax1.scatter(_x, _y, s=400, c='b', marker=r"$ {} $".format(_s))
+
+            plt.xlabel('F2')
+            plt.ylabel('F1')
+            plt.title('Vowel Predicted - Test accuracy: %.3f' % test_accuracy)
+            plt.savefig(plot_filename, bbox_inches='tight')
+
+            with open(plot_filename, "rb") as imageFile:
+                return base64.b64encode(imageFile.read())
+        except:
+            print "Error: ", sys.exc_info()
+            raise
+
+    def models_if_exist(self):
+        try:
+            path = os.path.dirname(os.path.abspath(__file__))
+
+            female_model = os.path.join(path, self.female_model_name)
+            male_model = os.path.join(path, self.male_model_name)
+
+            exist_female = os.path.exists(female_model)
+            exist_male = os.path.exists(male_model)
+
+            return (exist_female and exist_male)
+        except:
+            return False
---
+++ Server/api/goofy/views.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+The MIT License
+
+Copyright (c) 2015 University of Rochester, Uppsala University
+Authors: Davide Berdin, Philip J. Guo, Olle Galmo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+import base64
+import json
+import random
+import glob
+
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from .models import User, GeneralScore
+
+from machine_learning.prepare_data import *
+from machine_learning.GMM_system import GMM_prototype
+
+data_directory = "data/"
+
+
+# login process
+@api_view(['POST'])
+def login(request):
+    try:
+        if request.method == 'POST':
+            json_data = json.loads(request.body)
+            data = dict(json_data)
+
+            response = {}
+            all_users = User.objects.all()
+            for u in all_users:
+                if data['Username'] == u.username:
+                    response["Response"] = "SUCCESS"
+                    response["Username"] = u.username
+                    response["Password"] = u.password
+                    response["Gender"] = u.gender
+                    response["Nationality"] = u.nationality
+                    response["Occupation"] = u.occupation
+                    response["Score"] = get_score(u)
+
+                    sentence, phonetic = get_sentence()
+                    response["Sentence"] = sentence
+                    response["Phonetic"] = phonetic
+
+                    return HttpResponse(json.dumps(response))
+
+            response = {}
+            response["Response"] = "FAILED"
+            response["Reason"] = "Something went wrong during the authentication\n Try later"
+            return HttpResponse(json.dumps(response))
+    except:
+        print "Error: ", sys.exc_info()
+        raise
+
+
+def get_score(user):
+    try:
+        all_scores = GeneralScore.objects.all()
+        for sc in all_scores:
+            if user.id == sc.id:
+                return sc.total_score
+        return -1
+    except:
+        print "Error: ", sys.exc_info()
+        raise
+
+
+def get_sentence():
+    sentences = ["A piece of cake", "Blow a fuse", "Catch some zs", "Down to the wire", "Eager beaver",
+                 "Fair and square", "Get cold feet", "Mellow out", "Pulling your legs", "Thinking out loud"]
+    phonetic = ["ɐ pˈiːs ʌv kˈeɪk", "blˈoʊ ɐ fjˈuːz", "kˈætʃ sˌʌm zˌiːˈɛs", "dˌaʊn tə ðə wˈaɪɚ", "ˈiːɡɚ bˈiːvɚ",
+                "fˈɛɹ ænd skwˈɛɹ", "ɡɛt kˈoʊld fˈiːt", "mˈɛloʊ ˈaʊt", "pˈʊlɪŋ jʊɹ lˈɛɡz", "θˈɪŋkɪŋ ˈaʊt lˈaʊd"]
+    index = random.randrange(start=0, stop=len(sentences))
+
+    return sentences[index], phonetic[index]
+
+
+# Registration process
+@api_view(['POST'])
+def register(request):
+    try:
+        if request.method == 'POST':
+            json_data = json.loads(request.body)
+            data = dict(json_data)
+
+            response = {}
+            all_users = User.objects.all();
+            username = data['Username']
+
+            for u in all_users:
+                if u.username == username:
+                    response["Response"] = "FAILED"
+                    response["Reason"] = "Username already registered"
+                    return HttpResponse(json.dumps(response))
+
+            password = data['Password']
+            gender = data['Gender']
+            nationality = data['Nationality']
+            occupation = data['Occupation']
+
+            new_user = User(username=username, password=password, gender=gender, nationality=nationality,
+                            occupation=occupation)
+            new_user.save()
+
+            sentence, phonetic = get_sentence()
+
+            response["Response"] = "SUCCESS"
+            response["Sentence"] = sentence
+            response["Phonetic"] = phonetic
+
+            return HttpResponse(json.dumps(response))
+    except:
+        print "Error: ", sys.exc_info()
+        raise
+
+
+# noinspection PyTypeChecker
+@api_view(['POST'])
+def test_pronunciation(request):
+    try:
+        response = {}
+        if request.method != 'POST':
+            raise
+
+        path = os.path.dirname(os.path.abspath(__file__))
+        audio_path = path + "/audio/"
+
+        json_data = json.loads(request.body)
+        data = dict(json_data)
+
+        audiofile_string = data['FileAudio']
+        sentence = data['Sentence']
+
+        audiofile_byte = base64.b64decode(audiofile_string)
+        random_hash = random.getrandbits(128)
+
+        generate_filename = sentence.replace(' ', '_')
+        temp_audiofile = audio_path + generate_filename + "_" + str(random_hash) + ".wav"
+
+        with open(temp_audiofile, 'wb') as output:
+            output.write(audiofile_byte)
+
+        user_data = data['User']
+        user = json.loads(user_data)
+
+        # need to load the correct model - M or F
+        gender = user['gender']
+        response['Response'] = 'SUCCESS'
+
+        # TODO: remove the following line related to sentence becuase I'm still testing the same file
+        sentence = "A piece of cake"
+        response['VowelChart'] = classify_user_audio(temp_audiofile, sentence, gender)
+
+        # clean up everything
+        cleaned_name = temp_audiofile.replace('.wav', '*')
+
+        filelist = glob.glob(cleaned_name)
+        for filename in filelist:
+            os.remove(filename)
+
+        data_path = path + '/machine_learning/data/'
+        (dirName, fileName) = os.path.split(temp_audiofile)
+        cleaned_name =  data_path + fileName.replace('.wav', '*')
+
+        filelist = glob.glob(cleaned_name)
+        for filename in filelist:
+            os.remove(filename)
+
+        return HttpResponse(json.dumps(response))
+    except:
+        print "Error: ", sys.exc_info()
+
+        response['Response'] = 'FAILED'
+        response['Reason'] = sys.exc_info()
+        return HttpResponse(json.dumps(response))
+
+
+def classify_user_audio(audiofile, sentence, gender):
+    # Force Alignment
+    force_alignment(audiofile, sentence)
+
+    # FAVE-exctract
+    if gender == 'm':
+        extract_data(audiofile, True)
+    else:
+        extract_data(audiofile, False)
+
+    # Create GMM testing set
+    (dirName, fileName) = os.path.split(audiofile)
+    test_data = create_test_data(fileName)
+
+    # Test on GMM and get prediction
+    X_test, Y_test = create_test_set(test_data)
+    plot_filename = audiofile.replace('.wav', '.png')
+
+    gmm_obj = GMM_prototype()
+    if gender == 'm':
+        return gmm_obj.test_GMM(X_test, Y_test, plot_filename, False)
+    else:
+        return gmm_obj.test_GMM(X_test, Y_test, plot_filename, True)
