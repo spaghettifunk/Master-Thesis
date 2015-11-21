9bc97d3f6d93e88a3ecfeed0c40a51022c07c692
davideberdin <davide.berdin.0110@student.uu.se>
2015-11-21 18:47:06 -0500

    removed an import

--- Server/phonemes_service/src/controllers/Transcription.java
+++ Server/phonemes_service/src/controllers/Transcription.java
@@ -6,7 +6,6 @@ import org.json.*;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.io.*;
-import java.lang.reflect.Field;
 import java.util.UUID;
 
 import edu.cmu.sphinx.recognizer.Recognizer;
